64c2906c52856f425f3a99986b243d631bff422a
Katharina Biernacka <kath.biernacka@gmail.com>
2021-12-14 21:36:17 +0100

    flutter bug fixed

--- lib/screens/sports/sports.dart
+++ lib/screens/sports/sports.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 
-class SingleSport {
-  String sportTitle;
-  var record;
-  var today;
-  var isFavorite = false;
-  SingleSport(this.sportTitle, this.record, this.today, this.isFavorite);
+/// The base class for the different types of items the list can contain.
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
 }
 
-List<SingleSport> sportList = [];
+/// A ListItem that contains data to display a heading.
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.headline5,
+    );
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
+}
+
+/// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(sender);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
+}
+final items = List<ListItem>.generate(
+  1000,
+  (i) => i % 6 == 0
+      ? HeadingItem('Heading $i')
+      : MessageItem('Sender $i', 'Message body $i'),
+);
